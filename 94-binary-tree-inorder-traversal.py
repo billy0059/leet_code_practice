0580e23e9396beb645463ab128093d829d20b008
BENLI WANG <benliwang@BENLIs-MacBook-Air.local>
2023-07-11 20:36:31 +0800

    Add leetcode problem number in file name's prefix

--- 94-binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal.py
@@ -1,5 +1,5 @@
-#https://leetcode.com/problems/binary-tree-inorder-traversal/
-#Given the root of a binary tree, return the inorder traversal of its nodes' values.
+# https://leetcode.com/problems/binary-tree-inorder-traversal/
+# Given the root of a binary tree, return the inorder traversal of its nodes' values.
 from typing import Optional
 from typing import List
 
