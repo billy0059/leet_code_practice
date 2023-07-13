bc40462017f189261809b634daae805701c6aae5
BENLI WANG <benliwang@BENLIs-MacBook-Air.local>
2023-07-13 23:56:51 +0800

    More efficient way for the anagram ID, use "list to tuple" instead of "dict to tuple"

--- 49-group-anagrams.py
+++ 49-group-anagrams.py
@@ -1,8 +1,8 @@
 class Solution:
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        '''
         if len(strs) == 0:
             return [[""]]
-        str_anagram_map_list=[]
         return_map=collections.defaultdict(list)
         for str in strs:
             anagram_map = collections.defaultdict(int)
@@ -13,5 +13,18 @@ class Solution:
             # A dict can be a key in another dict if you
             # First sort the dict with "sorted(dict.item())" and make it tuple:
             # tuple(sorted(dict.items()))
+
             return_map[tuple(sorted(anagram_map.items()))].append(str)
         return return_map.values()
+        '''
+
+        #######################################################################################
+        # More efficient way for the hash map key is a tuple of list, instead of sorted dict. #
+        #######################################################################################
+        ret_anagram_map = collections.defaultdict(list)
+        for str in strs:
+            anagram_id = [0] * 26 # This is the anagram ID for the string
+            for c in str:
+                anagram_id[ord(c) - ord("a")] += 1
+            ret_anagram_map[tuple(anagram_id)].append(str)
+        return ret_anagram_map.values()
