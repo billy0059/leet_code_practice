b1c7649fcad42152da88293a10841174bb9a44aa
BENLI WANG <benliwang@BENLIs-MacBook-Air.local>
2023-07-13 23:28:17 +0800

    Add some note for anagram problem: You can not just determine anagram with set. You also need to care about the numbers of character in the string

--- 242-valide-anagram.py
+++ 242-valide-anagram.py
@@ -1,7 +1,11 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         # One line solution
+        # XXXXXXXXXXXXX Do not write this XXXXXXXXXXXXX
         # return set(s) == set(t)
+        # XXXXXXXXXXXXX Do not write this XXXXXXXXXXXXX
+        # Above is wrong! you can not just compare the set of the string.
+        # For example, "ggd", "gdd", they are not anagram but they share same set (g,d).
         char_times_dict = collections.defaultdict(int)
         for char in s:
             char_times_dict[char] += 1
